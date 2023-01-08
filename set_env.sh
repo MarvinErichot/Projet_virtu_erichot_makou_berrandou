a7329e8b3ce25d4027e1c7fffb1f1d9a3df0202c
Marvin Erichot <115141441+st4ssx@users.noreply.github.com>
2023-01-08 21:28:41 +0100

    Add files via upload

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+
+
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres-hIMV
+export POSTGRES_PASSWORD=postgrespw
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=postgres-hIMV
+export AWS_BUCKET=udagram-esiea-8765
+export AWS_REGION=eu-west-3
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
